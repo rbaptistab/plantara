9f02b939e9d5cbf0d3a72c04444a7376c0e0785a
raquel <raquelburgerpsi@gmail.com>
2023-11-23 09:31:37 -0300

    comiitando atualizacoes

--- pages/paginaHorta.php
+++ pages/paginaHorta.php
@@ -7,9 +7,7 @@ if (isset($_GET['id'])) {
 } else {
     echo "Horta não encontrada";
 }
-$url = 'http://ec2-3-144-163-238.us-east-2.compute.amazonaws.com:8080/usuario/3';
-$response = file_get_contents($url);
-$data = json_decode($response, true);
+
 
 $urlHorta = 'http://ec2-3-144-163-238.us-east-2.compute.amazonaws.com:8080/horta';
 $responseHorta = file_get_contents($urlHorta);
@@ -19,23 +17,44 @@ $urlImgHorta = 'http://ec2-3-144-163-238.us-east-2.compute.amazonaws.com:8080/ho
 
 ?>
 
+<html>
 
-<?php foreach ($dataHorta as $horta): ?>
-    <?php if ($horta['id'] == $hortaId): ?>
-        <h2>
-            <?php echo $horta['nomeHorta']; ?>
-        </h2>
-        <p>Localização:
-            <?php echo $horta['latitude'] . ', ' . $horta['longitude']; ?>
-        </p>
-        <p>Proprietário:
-            <?php echo $horta['usuario']['nome']; ?>
-        </p>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2">
+                <?php require_once("../templates/menu.php"); ?>
+            </div>
+            <div class="col-md-10 titulo-page">
+                <div>
+                    <?php require_once("../templates/header.php"); ?>
+                </div>
 
-        <?php foreach ($horta['imagems'] as $imagem): ?>
-            <img src="<?php echo $urlImgHorta . $imagem['ds_nome']; ?>" alt="<?php echo $imagem['ds_nome']; ?>"
-                style="width: 200px; margin: 5px;">
-        <?php endforeach; ?>
+                <?php foreach ($dataHorta as $horta): ?>
+                    <?php if ($horta['id'] == $hortaId): ?>
+                        <h2>
+                            <?php echo $horta['nomeHorta']; ?>
+                        </h2>
+                        <p>Localização:
+                            <?php echo $horta['latitude'] . ', ' . $horta['longitude']; ?>
+                        </p>
+                        <p>Proprietário:
+                            <?php echo $horta['usuario']['nome']; ?>
+                        </p>
 
-    <?php endif; ?>
-<?php endforeach; ?>
+                        <?php foreach ($horta['imagems'] as $imagem): ?>
+                            <img src="<?php echo $urlImgHorta . $imagem['ds_nome']; ?>" alt="<?php echo $imagem['ds_nome']; ?>"
+                                style="width: 200px; margin: 5px;">
+                        <?php endforeach; ?>
+
+                    <?php endif; ?>
+                <?php endforeach; ?>
+
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
